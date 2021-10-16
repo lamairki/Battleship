0bfb283c1acf156f35a6adba7db30f3ae53a91b8
Kirk LaMaire <lamairki@mail.gvsu.edu>
2021-10-16 11:25:07 -0400

    initial commit with very basic board.hpp file and ship.hpp file

---
+++ board.hpp
@@ -0,0 +1,28 @@
+#ifndef         __H_BOARD__
+#define         __H_BOARD__
+
+const int BOARD_HEIGHT = 10;
+const int BOARD_WIDTH = 10;
+
+enum rowNames {
+	A,
+	B,
+	C,
+	D,
+	E,
+	F,
+	G,
+	H,
+	I,
+	J
+};
+
+/** This function will take in the vertical and
+  * horizontal coordinates desired along with 
+  * the cardinal direction the ship should
+  * face and the ship, then adds it to the board.
+**/
+
+
+
+#endif
---
+++ ship.hpp
@@ -0,0 +1,12 @@
+#ifndef         __H_SHIP__
+#define         __H_SHIP__
+
+enum shipType {
+	CARRIER,
+	BATTLESHIP,
+	CRUISER,
+	SUBMARINE,
+	DESTROYER
+};
+
+#endif
